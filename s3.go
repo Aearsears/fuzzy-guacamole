dcee2e0c396b8554f4cab3af56b5857faf9a5f23
Aearsears <53102185+Aearsears@users.noreply.github.com>
2025-04-14 22:24:44 -0400

    start on s3 menu

--- s3.go
+++ s3.go
@@ -92,9 +92,6 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-
 		case "up":
 			if m.selected > 0 {
 				m.selected--
